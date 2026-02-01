03b0724975227a4c8398233f739ce827dd2c9acc
abdeahmed <a.khalfalla151@gmail.com>
2026-02-01 05:22:55 +0300

    deployment in process.

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/graphql/',
+  base: './',
   plugins: [
     devtools(),
     tanstackRouter({
